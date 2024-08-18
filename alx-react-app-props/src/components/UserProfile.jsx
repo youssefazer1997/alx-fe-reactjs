24ccbcb0e32233226879d51c8e1f0811d10868e7
youssefazer97 <yoosha1997@gmail.com>
2024-08-19 02:08:19 +0300

    Week 10 - Task 2-8

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,7 +1,11 @@
+import React, { useContext } from "react";
+import { UserContext } from "./components/UserContext.js";
+
 function UserProfile(props) {
+  const [name] = useContext(UserContext);
   return (
     <div>
-      <h2>{props.name}</h2>
+      <h2>{name}</h2>
       <p>Age: {props.age}</p>
       <p>Bio: {props.bio}</p>
     </div>
