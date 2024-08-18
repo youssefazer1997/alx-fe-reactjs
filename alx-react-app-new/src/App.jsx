604e00a5fb22e288ed4aac06b50b53bc89336778
youssefazer97 <yoosha1997@gmail.com>
2024-08-18 23:11:59 +0300

    Week 10 - Task 1

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
 function App() {
   const [count, setCount] = useState(0);
 
@@ -30,6 +31,7 @@ function App() {
         age="27"
         bio="Loves Ultimate Frisbee and Pen Plotting"
       />
+      <Counter />
       <Footer />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
