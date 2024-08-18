ccabfb3d83d6409a759dfb33feb77f989dd78e84
youssefazer97 <yoosha1997@gmail.com>
2024-08-19 01:27:36 +0300

    Week 10 - Task 2-3

--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { UserContext } from "./UserContext";
+import { UserContext } from "./Contexts/UserContext";
 
 function UserDetails() {
   const { name, email } = useContext(UserContext);
