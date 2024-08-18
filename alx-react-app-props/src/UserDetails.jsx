2138bf4e14fb28b2398126df3fdd59acef21b596
youssefazer97 <yoosha1997@gmail.com>
2024-08-19 01:01:13 +0300

    Week 10 - Task 2-1

---
+++ alx-react-app-props/src/UserDetails.jsx
@@ -0,0 +1,12 @@
+import React, { user } from "react";
+
+function UserDetails({ userData }) {
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;
