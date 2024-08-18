4b7bd9f76a7acf620966e7abd2683d628b8d7abd
youssefazer97 <yoosha1997@gmail.com>
2024-08-18 22:52:52 +0300

    Week 10 - Task 0

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <main>
+      <p style={{ fontSize: "30px", fontWeight: "bold" }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
