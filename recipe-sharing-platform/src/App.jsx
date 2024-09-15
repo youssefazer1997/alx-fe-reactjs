01c91920e3b97c3efba61302ee1a9368e077b570
youssefazer97 <yoosha1997@gmail.com>
2024-09-15 18:27:26 +0300

    Week 14 Task 0-1

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 class="text-blue-600">Vite + React</h1>
+      <h1 class="text-blue-400">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
