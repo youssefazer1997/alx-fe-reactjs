15bd75d96cf5e3a5e9bc64fb64f2c38322d4957a
youssefazer97 <yoosha1997@gmail.com>
2024-09-15 18:15:55 +0300

    Week 14 Task 0-0

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
